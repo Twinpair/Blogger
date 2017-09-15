db8688a952a7cf027025c70c1d1c671de577d2ea
Eric <twinpaireric@gmail.com>
2017-09-15 15:35:25 -0700

    Change root route to articles index

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Blogger::Application.routes.draw do
-  root to: 'author_sessions#new'
+  root to: 'articles#index'
 
   resources :articles do
     resources :comments
   end
 
   resources :tags
-
   resources :authors
-
   resources :author_sessions, only: [ :new, :create, :destroy ]
 
   get 'login'  => 'author_sessions#new'
